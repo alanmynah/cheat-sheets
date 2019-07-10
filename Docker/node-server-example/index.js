8110757468d1bed743f8e5715cbfc022f7090de1
AM <michael@Internet.local>
2019-07-10 19:01:12 +0100

    Make the example clearer

--- Docker/node-server-example/index.js
+++ Docker/node-server-example/index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hi there');
+  res.send('Hello from node docker example');
 });
 
 app.listen(8080, () => {
-  console.log('Listening on 8080');
+  console.log('This example is waiting for you on localhost:8080');
 });
