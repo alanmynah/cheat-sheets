30016e0bd384a5b6f40ca671367e0be11bd6b1ea
AM <michael.rybintsev@gmail.com>
2019-07-11 17:36:49 +0100

    Slight improvements to build speed

--- Docker/node-server-example/index.js
+++ Docker/node-server-example/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from node docker example');
+  res.send('Hello from node-example');
 });
 
 app.listen(8080, () => {
