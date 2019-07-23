a615a89eea766ce00a4f8aca90a25d02ec27ca6e
AM <michael.rybintsev@gmail.com>
2019-07-23 07:38:10 +0100

    rename last part in dev-prod-example

--- Docker/dev-prod-container-example/frontend/src/App.js
+++ Docker/dev-prod-container-example/frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
             target='_blank'
             rel='noopener noreferrer'
           >
-            fullstack-example.
+            dev-prod-container-example.
           </a>
         </p>
         <p>
